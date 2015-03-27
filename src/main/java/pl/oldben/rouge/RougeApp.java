fec67804e22c8bf0ca77f551cc02fffca2ce6f1b
fortisprog <fortisprog@gmail.com>
2015-03-28 00:00:13 +0100

    added slick and lwjgl files to gradle and a "hello world" example code

--- src/main/java/pl/oldben/rouge/RougeApp.java
+++ src/main/java/pl/oldben/rouge/RougeApp.java
@@ -1,11 +1,40 @@
 package pl.oldben.rouge;
 
-public class RougeApp {
-    public static void main(String[] args){
-        new RougeApp().run();
-    }
 
-    private void run() {
-        System.out.println("Hello");
-    }
+import org.newdawn.slick.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class RougeApp extends BasicGame {
+	private static final Logger LOGGER = LoggerFactory.getLogger(RougeApp.class);
+
+	RougeApp(String gamename) {
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+	}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		g.drawString("Hello Slick", 50, 50);
+	}
+
+	public static void main(String[] args) {
+		try {
+			AppGameContainer appgc = new AppGameContainer(new RougeApp("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+
+		} catch (SlickException ex) {
+			LOGGER.error("Cannot start application", ex);
+
+		}
+	}
 }
